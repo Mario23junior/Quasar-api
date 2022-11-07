f830b53a9102774dddbbd647dee5baa9971022d7
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-11-07 02:40:45 -0300

    Criando classe service de quasar

---
+++ src/main/java/com/api/quasar/service/QuasarService.java
@@ -0,0 +1,23 @@
+package com.api.quasar.service;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+import javax.ws.rs.core.Response;
+
+import com.api.quasar.dto.QuasarsDTO;
+import com.api.quasar.repository.QuasaresRepository;
+
+@ApplicationScoped
+public class QuasarService {
+  
+	private QuasaresRepository repository;
+	
+	public QuasarService(QuasaresRepository repository) {
+		this.repository = repository;
+ 	}
+  
+	@Transactional
+	public Response save(QuasarsDTO quasarsDto) {
+		
+	}
+}
