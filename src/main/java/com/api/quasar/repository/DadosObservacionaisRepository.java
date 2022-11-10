afb93e80db165cb32ae3164501c14898c84de436
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-11-10 01:20:23 -0300

    Criando consulta customizando de nomes de costelações.

--- src/main/java/com/api/quasar/repository/DadosObservacionaisRepository.java
+++ src/main/java/com/api/quasar/repository/DadosObservacionaisRepository.java
@@ -1,5 +1,7 @@
 package com.api.quasar.repository;
 
+import java.util.Optional;
+
 import javax.enterprise.context.ApplicationScoped;
 
 import com.api.quasar.model.DadosObservacionais;
@@ -8,5 +10,9 @@ import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 
 @ApplicationScoped
 public class DadosObservacionaisRepository implements PanacheRepositoryBase<DadosObservacionais, Long>{
+
+	public Optional<DadosObservacionais> findByName(String nomeConstalacao) {
+		return find("nomeConstalacao",nomeConstalacao.toUpperCase().toLowerCase()).firstResultOptional();
+	}
 
 }
